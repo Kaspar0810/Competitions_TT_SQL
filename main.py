609692e405eba53bd7c736e57b9ed7036a5d1248
Kaspar0810 <al.kabanov61@gmail.com>
2022-04-13 16:42:57 +0300

    жеребьевка регионов без участников

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 
 
 
-from imp import load_dynamic
 from reportlab.pdfbase.pdfmetrics import registerFontFamily
 from reportlab.platypus import PageBreak
 from reportlab.lib.styles import ParagraphStyle as PS, getSampleStyleSheet
@@ -3773,20 +3772,18 @@ def choice_table():
 def test_choice_group():
     "новая система жеребьевки групп"
     " current_region_group - словарь (регион - список номеров групп куда можно сеять)"
+    load_tableWidget()
     posev_tmp = {}
+    player_posev_tmp = {}
     gr_region = {}
     posev_group = {}
-    id_list = []
+    player_id = []
     pgt = []
     posev = {}
     group_list = []
     start = 0
     end = 1
     step = 0
-    tmp = []
-    n = []
-
-    load_tableWidget()
 
     sys = System.select().where(System.title_id == title_id())
     sys_id = sys.select().where(System.stage == "Предварительный").get()
@@ -3805,29 +3802,25 @@ def test_choice_group():
     smena  = 0
     number_poseva = 0  # общий счетчик посева игроков
     reg_list = []
+    player_list = []
     for np in pl_choice:
         choice = np.get(Choice.id == np)
-        player_id = choice.player_choice_id
         region = choice.region
+        pl_id = choice.player_choice_id
         reg = Region.get(Region.region == region)
         region_id = reg.id 
         reg_list.append(region_id)
-        id_list.append(player_id)
+        player_list.append(pl_id)
     for p in range(1, total_player + 1):  # цикл по регионам жеребьевки
     
         for e in range(1, group + 1):  # получение списка всех групп
             group_list.append(e)
         region_id = reg_list[number_poseva]
-        #========================
-        pl_id = id_list[number_poseva]
-        #========================
+        pl_id = player_list[number_poseva]
         posev_tmp = posev[f"{m}_посев"]
-        if m == 1:  # 1-й посев    
+        if m == 1:  # 1-й посев        
             posev_tmp[p] = region_id  # создает словарь группа - номер региона
-            tmp.append(p)
-            tmp.append(pl_id)
-            n = tmp.copy()
-
+            player_posev_tmp[p] = pl_id
             number_poseva += 1
         else:  # 2-й посев и т.д.
             current_region_group = {}  # словарь регион - список номеров групп куда можно сеять
@@ -3865,12 +3858,13 @@ def test_choice_group():
                 previous_region_group = posev_test(posev, group, m)  # возвращает словарь регион  - список номера групп, где он есть
         else:
             print(posev)
-            return
-            with db:  # запись в таблицу Choice результата жеребъевки
-                grp = Choice.get(Choice.id == choice_id)
-                grp.group = txt
-                grp.posev_group = k
-                grp.save()
+            for y in range(1, total_player + 1):
+                txt = str(f'{i + y} группа')
+                with db:  # запись в таблицу Choice результата жеребъевки
+                    grp = Choice.get(Choice.id == choice_id)
+                    grp.group = txt
+                    grp.posev_group = k
+                    grp.save()
         group_list.clear()
 
 
@@ -3969,7 +3963,7 @@ def posev_test(posev, group, m):
 def choice_gr_automat():
     """проба автоматической жеребьевки групп, записывает в таблицу Choice номер группы и посев"""
     load_tableWidget()
- 
+    # gamer = my_win.lineEdit_title_gamer.text()
     sys = System.get(System.title_id == title_id()
                      and System.stage == "Предварительный")
     s_id = sys.id
