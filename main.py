8ed95b52f70e1eae691ee9c6c4a3a7882fff0ac8
aleksandr <al.kabanov61@gmail.com>
2024-03-12 21:49:31 +0300

    добавлен комбо по тренерам

--- main.py
+++ main.py
@@ -1885,7 +1885,7 @@ def input_player():
             full_stroka = f"{pl.r1_fname}, {str(pl.r1_list)}, {pl.r1_bithday}, {pl.r1_city}"
             my_win.listWidget.addItem(full_stroka) # заполняет лист виджет спортсменами
         reply = msgBox.question(my_win, 'Уведомление', "Если это новый игрок\nнажмите -Yes-\n"
-        "и заполните его данные,\nа если он появился\nв январском рейтинге\n нажмите -Cancel-",
+        "и заполните его данные,\nа если он есть\nв январском рейтинге\n нажмите -Cancel-",
                                                   msgBox.Yes,
                                                   msgBox.Cancel)
         if reply == msgBox.Yes:
